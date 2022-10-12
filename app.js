49ca1b2f39b2b5431a2534142636286188658935
Prateek Suman <prateeksuman51@gmail.com>
2022-10-12 21:05:42 +0530

    added api

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 var btnTranslate = document.querySelector("#btn-translate");
-function clickEventHandler(){
-    console.log("clicked");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+
+var serverURL ="https://api.funtranslations.com/translate/yoda.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
 }
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something wrong with the server! try again after sometime")
+}
+
+
+function clickEventHandler(){
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(responce=>responce.json())
+    .then(json=>{
+        var translatedText = json.contents.translated;
+        txtOutput.innerText = translatedText;
+    })
+    .catch(errorHandler)
+
+};
 
 document.addEventListener("click", clickEventHandler)
