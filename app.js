f911ca78402c8ddff99edc9c7c3644b5b4c243c8
Prateek Suman <prateeksuman51@gmail.com>
2022-11-17 03:57:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,28 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
 
-var serverURL ="https://api.funtranslations.com/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text=" + text
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
 }
 
-function errorHandler(error){
-    console.log("error occured",error);
-    alert("Something wrong with the server! try again after sometime")
+function errorHandler(error) {
+  alert("Something wrong with the server! try again after sometime");
 }
 
-
-function clickEventHandler(){
-    var inputText = txtInput.value;
-    fetch(getTranslationURL(inputText))
-    .then(responce=>responce.json())
-    .then(json=>{
-        var translatedText = json.contents.translated;
-        txtOutput.innerText = translatedText;
+function clickEventHandler() {
+  var inputText = txtInput.value;
+  fetch(getTranslationURL(inputText))
+    .then((responce) => responce.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      txtOutput.innerText = translatedText;
     })
-    .catch(errorHandler)
+    .catch(errorHandler);
+}
 
-};
-
-document.addEventListener("click", clickEventHandler)
+document.addEventListener("click", clickEventHandler);
